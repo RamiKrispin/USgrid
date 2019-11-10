6696bbd1ff531cc05ed0dd6e4594f5753972acca
RamiKrispin <ramkrisp@umich.edu>
2019-11-10 08:56:54 -0800

    Add new dataset

--- R/data.R
+++ R/data.R
@@ -11,7 +11,7 @@
 #' @source US Energy Information Administration (Nov 2019) \href{https://www.eia.gov/}{website}
 #' @keywords datasets, time-series
 #' @details The dataset contains the hourly demand and supply (generation) for electricity in the US (megawatthours).
-#' The `type` column describe the type of the series (demand or generation)
+#' The `type` column describes the type of the series (demand or generation)
 #' @examples
 #'
 #' data(US_elec)
@@ -27,3 +27,34 @@
 #'
 
 "US_elec"
+
+
+#' The US Hourly Net Generation by Energy Source
+
+#' @description The net generation of electricity in the US by energy source (i.e., natural gas, coal, solar, etc.) since July 2018.
+#'
+#' Units: megawatthours
+#'
+#' Time zone: UTC
+#'
+#'
+#' @format A tsibble object with hourly intervals
+#' @source US Energy Information Administration (Nov 2019) \href{https://www.eia.gov/}{website}
+#' @keywords datasets, time-series
+#' @details The dataset contains the hourly demand and supply (generation) for electricity in the US (megawatthours).
+#' The `source` column describes the type of the energy source
+#' @examples
+#'
+#' data(US_source)
+#'
+#' library(plotly)
+#'
+#' plot_ly(data = US_source,
+#'         x = ~ date_time,
+#'         y = ~ series,
+#'         color = ~ source,
+#'         type = "scatter",
+#'         mode = "lines")
+#'
+
+"US_source"
