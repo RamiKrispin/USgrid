92070dddf519218d53c6af3317e0d9aec74b94f2
--local <--local>
2019-11-10 07:16:26 -0800

    Update the series description

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' The US Hourly Demand and Supply for Electricity
+#'
+#' @description  The total demand and supply for electricity in the US since July 2015
+#'
+#' Units: megawatthours
+#'
+#' Time zone: UTC
+#'
+#'
+#' @format Data frame with timestamp (half-hour intervals)
+#' @source The UK natioanl grid \href{https://www.nationalgrid.com/uk}{website}
+#' @keywords datasets
+#' @details Field descriptions, source National Grid UK \href{https://www.nationalgrid.com/uk}{website}
