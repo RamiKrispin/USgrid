531aab5ad10ead0fe22ad588440d1f3cf7989728
--local <--local>
2019-11-10 07:30:56 -0800

    Updating the dataset doc

--- R/data.R
+++ R/data.R
@@ -1,13 +1,27 @@
 #' The US Hourly Demand and Supply for Electricity
 #'
-#' @description  The total demand and supply for electricity in the US since July 2015
+#' @description  The total hourly demand and supply (generation) for electricity in the US since July 2015
 #'
 #' Units: megawatthours
 #'
 #' Time zone: UTC
 #'
 #'
-#' @format Data frame with timestamp (half-hour intervals)
-#' @source The UK natioanl grid \href{https://www.nationalgrid.com/uk}{website}
-#' @keywords datasets
-#' @details Field descriptions, source National Grid UK \href{https://www.nationalgrid.com/uk}{website}
+#' @format A tsibble object with hourly intervals
+#' @source US Energy Information Administration (Nov 2019) \href{https://www.eia.gov/}{website}
+#' @keywords datasets, time-series
+#' @details The dataset contains the hourly demand and supply (generation) for electricity in the US (megawatthours).
+#' The `type` column describe the type of the series (demand or generation)
+#' @examples
+#'
+#' data(US_elec)
+#'
+#' library(plotly)
+#'
+#' plot_ly(data = US_elec,
+#'         x = ~ date_time,
+#'         y = ~ series,
+#'         color = ~ type,
+#'         type = "scatter",
+#'         mode = "lines")
+#'
